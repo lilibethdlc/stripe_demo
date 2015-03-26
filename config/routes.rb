b36fb853c9fc1e0de43033e580222816c62e2815
Lilibeth De La Cruz <lilibethdlc@gmail.com>
2015-03-26 13:11:30 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get 'home#custom', to: 'home#custom', as: :custom
+  get 'custom', to: 'home#custom', as: :custom
 
   post 'charge_index', to: 'home#charge_index', as: :charge_index
   post 'charge_custom', to: 'home#charge_custom', as: :charge_custom
